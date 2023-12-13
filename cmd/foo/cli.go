17ed7dd47a14f6969c1baf268c6e4482848f5a92
Mike Fridman <mf192@icloud.com>
2023-12-12 22:44:17 -0500

    wip

--- cmd/foo/cli.go
+++ cmd/foo/cli.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"runtime/debug"
 
 	"github.com/peterbourgon/ff/v4"
 )
 
 func Run() {
+	info, ok := debug.ReadBuildInfo()
+	fmt.Println(info, ok)
+	by, err := json.MarshalIndent(info, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(by))
 	_ = ff.Command{}
 	fmt.Println("Hello, World!")
 }
